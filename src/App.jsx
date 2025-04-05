877faca9788449111a1970ef6b5180e8231cae62
Manish Awtani <manishawtani01@gmail.com>
2025-04-06 02:54:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black text-black dark:text-white">
+        <h1 className="text-3xl font-bold">Hello, Manish Awtani 👋</h1>
+        <h2 className="text-4xl font-semibold font-sans">
+          Welcome to My Portfolio
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App
