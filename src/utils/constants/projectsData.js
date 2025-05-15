75988a60f16f681fcf05d07b86b4b6e1ecb3975c
Manish Awtani <manishawtani01@gmail.com>
2025-05-15 21:55:37 +0530

    experience + project data

---
+++ src/utils/constants/projectsData.js
@@ -0,0 +1,33 @@
+const projectsData = [
+  {
+    title: "gauri BI",
+    year: 2022,
+    video: "/src/assets/project-videos/pure2.0-2x.mp4",
+    simulatorType: "mobile",
+    link: "https://your-app-link.com",
+  },
+  {
+    title: "bolt.new",
+    year: 2022,
+    video: "/src/assets/project-videos/bolt.newer-demo.mp4",
+    simulatorType: "desktop",
+    link: "https://github.com/manish-awtani/bolt.newer",
+  },
+  {
+    title: "pure 2.0",
+    year: 2022,
+    video: "/src/assets/project-videos/vid-2.mp4",
+    simulatorType: "mobile",
+    link: "https://your-other-app-link.com",
+  },
+  {
+    title: "/Gifu",
+    year: 2022,
+    video: "/videos/gif-demo.mp4",
+    simulatorType: "mobile",
+    link: "https://your-other-app-link.com",
+  },
+  // Add more projects
+];
+
+export default projectsData;
